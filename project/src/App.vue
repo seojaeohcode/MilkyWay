a63d3f1edc4a5834e260a47c703a41106b13fb35
seojaeohcode <seojaeohcoder@gmail.com>
2022-07-12 13:56:39 +0900

    commit

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,21 +1,25 @@
-<script setup>
+<script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Head from "./components/layout/Header.vue";
+
+export default {
+    name: "App",
+    components: {
+        Head,
+    },
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <div id="app">
+        <Head></Head>
+        <div id="content" class="content">
+            <router-view></router-view>
+        </div>
+    </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ projectWeb/src/components/layout/Header.vue
@@ -0,0 +1,9 @@
+<template>
+<b-button variant="primary">Button</b-button>
+</template>
+
+<script>
+export default {
+    name: "header"
+};
+</script>
