f766eb627ca676b7fd32d23f60184a0a7aed6e79
seojaeohcode <seojaeohcoder@gmail.com>
2022-07-13 16:49:38 +0900

    commit

--- milky-way/src/App.vue
+++ milky-way/src/App.vue
@@ -5,20 +5,22 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+<!-- 안 바뀌는 정적 헤더(fixed vue와 dynamic vue를 정리) -->
   <div id="nav">
       <router-link to="/">Header</router-link>
       <router-link to="/award">Award</router-link>
   </div>
   <router-view />
+  <!--<HelloWorld></HelloWorld>-->
 </template>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
