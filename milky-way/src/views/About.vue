7fc9e5eeaf296b79261ab3a9cb0ea70d99c2598d
seojaeoh <seojaeohcode@gmail.com>
2022-07-29 08:36:48 +0900

    tablet&pc complete

--- milky-way/src/views/About.vue
+++ milky-way/src/views/About.vue
@@ -1,20 +1,22 @@
 <template>
     <div id="wrap">
-        <div>
-        
-        </div>
-        
-        <div>
-        
-        </div>
-        
-        <div>
-        
-        </div>
+
     </div>
 </template>
 
 <style scoped>
 /* About까지는 미디어쿼리, 반응형 조정까지 */
 /* Lab에서는 슬라이드 쇼 + InnerHtml */
+
+/* 모바일 320px부터 체크*/
+@media (max-width:767px) {
+    #wrap{
+        
+    }
+}
+/* 태블릿 */
+@media (min-width:768px) and (max-width:1024px) {}
+/* PC */
+@media (min-width:1025px) {}
+
 </style>
