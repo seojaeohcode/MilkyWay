081c29ad0c9226225a3e8b8e23a20e77b8f8a708
seojaeoh <seojaeohcode@gmail.com>
2022-07-29 15:33:48 +0900

    add_svg

--- milky-way/src/views/About.vue
+++ milky-way/src/views/About.vue
@@ -1,6 +1,26 @@
 <template>
     <div id="wrap">
+        <div id="linktext">
+            ABOUT
+        </div>
 
+        <!-- 2개는 서로 float -->
+        <div id="zodiacimg">
+            <img src="../assets/zodiac/aquarius.svg" alt="">
+        </div>
+        <div id="explain">
+            <div>
+
+            </div>
+            
+            <div>
+
+            </div>
+            
+            <div>
+            
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,15 +28,60 @@
 /* About까지는 미디어쿼리, 반응형 조정까지 */
 /* Lab에서는 슬라이드 쇼 + InnerHtml */
 
+/* Libre */
+@import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@1&display=swap');
+
 /* 모바일 320px부터 체크*/
 @media (max-width:767px) {
-    #wrap{
-        
+    #wrap {
+        height: fit-content;
+        margin-top: 1vh;
+    }
+
+    #linktext {
+        color: white;
+        font-size: 8vw;
+        text-align: center;
+        font-family: 'Libre Baskerville', serif;
+    }
+
+    #zodiacimg {
+        width: fit-content;
+        margin: 3rem auto;
+    }
+
+    #zodiacimg>img {
+        width: 60vw;
     }
 }
-/* 태블릿 */
-@media (min-width:768px) and (max-width:1024px) {}
-/* PC */
-@media (min-width:1025px) {}
 
+/* 태블릿 */
+@media (min-width:768px) and (max-width:1024px) {
+    #wrap {
+        height: fit-content;
+        margin-top: 1vh;
+    }
+
+    #linktext {
+        color: white;
+        font-size: 8vw;
+        text-align: center;
+        font-family: 'Libre Baskerville', serif;
+    }
+}
+
+/* PC */
+@media (min-width:1025px) {
+    #wrap {
+        height: fit-content;
+        margin-top: 1vh;
+    }
+
+    #linktext {
+        color: white;
+        font-size: 8vw;
+        text-align: center;
+        font-family: 'Libre Baskerville', serif;
+    }
+}
 </style>
